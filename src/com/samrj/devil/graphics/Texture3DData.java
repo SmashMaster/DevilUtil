ff0b3c4ccd0d8dc65727332373eac4102d134604
Sam Johnson <x.smashmaster.x@gmail.com>
2014-09-09 22:38:16 -0600

    Added 3D texture support
    Added GLTexture3D, Texture3DData, and Texture3DParams

---
+++ src/com/samrj/devil/graphics/Texture3DData.java
@@ -0,0 +1,70 @@
+package com.samrj.devil.graphics;
+
+import com.samrj.devil.buffer.Buffer;
+import com.samrj.devil.buffer.ByteBuffer;
+import java.awt.image.Raster;
+
+/**
+ * 3D texture data class for OpenGL.
+ * 
+ * @author Samuel Johnson (SmashMaster)
+ * @copyright 2014 Samuel Johnson
+ * @license https://github.com/SmashMaster/DevilUtil/blob/master/LICENSE
+ */
+public class Texture3DData
+{
+    public final int width, height, depth, format, baseFormat, bands;
+    private ByteBuffer buffer;
+    
+    public Texture3DData(int width, int height, int depth, int format, Buffer b)
+    {
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+        this.format = format;
+        baseFormat = TexUtil.getBaseFormat(format);
+        bands = TexUtil.getBands(baseFormat);
+        if (bands == -1) throw new IllegalArgumentException("Illegal format specified.");
+        
+        int length = width*height*depth*bands*b.getType().size;
+        if (b.size() != length) throw new IllegalArgumentException("Illegal input buffer size " + b.size() + ", expected size " + length);
+        buffer = new ByteBuffer(length);
+        b.get();
+        buffer.put(b.byteBuffer());
+    }
+    
+    public Texture3DData(int width, int height, int depth, int format)
+    {
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+        this.format = format;
+        baseFormat = TexUtil.getBaseFormat(format);
+        bands = TexUtil.getBands(baseFormat);
+        if (bands == -1) throw new IllegalArgumentException("Illegal format specified.");
+        
+        int length = width*height*depth*bands;
+        buffer = new ByteBuffer(length);
+        buffer.put(new byte[length]);
+    }
+    
+    /**
+     * Prepares the underlying buffer to be read entirely.
+     * 
+     * @return the java.nio buffer associated with this RasterBuffer.
+     */
+    public java.nio.ByteBuffer read()
+    {
+        return buffer.get();
+    }
+    
+    public GLTexture3D makeTexture3D(Texture3DParams params)
+    {
+        return new GLTexture3D(this, params);
+    }
+    
+    public GLTexture3D makeTexture3D()
+    {
+        return new GLTexture3D(this);
+    }
+}
--- src/com/samrj/devil/graphics/Texture3DParams.java
+++ src/com/samrj/devil/graphics/Texture3DParams.java
@@ -1,25 +1,28 @@
 package com.samrj.devil.graphics;
 
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
 
 /**
- * Contains texture parameters for GLTexture2D.
+ * Contains texture parameters for GLTexture3D.
  * 
  * @author Samuel Johnson (SmashMaster)
  * @copyright 2014 Samuel Johnson
  * @license https://github.com/SmashMaster/DevilUtil/blob/master/LICENSE
  */
-public final class Texture2DParams
+public final class Texture3DParams
 {
     public int wrapS = GL11.GL_REPEAT;
     public int wrapT = GL11.GL_REPEAT;
+    public int wrapR = GL11.GL_REPEAT;
     public int minFilter = GL11.GL_LINEAR;
     public int magFilter = GL11.GL_LINEAR;
     
-    public void glApply(GLTexture2D texture)
+    public void glApply(GLTexture3D texture)
     {
         texture.glParam(GL11.GL_TEXTURE_WRAP_S, wrapS);
         texture.glParam(GL11.GL_TEXTURE_WRAP_T, wrapT);
+        texture.glParam(GL12.GL_TEXTURE_WRAP_R, wrapR);
         texture.glParam(GL11.GL_TEXTURE_MIN_FILTER, minFilter);
         texture.glParam(GL11.GL_TEXTURE_MAG_FILTER, magFilter);
     }
