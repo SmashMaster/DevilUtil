e8e92698ab3ae0af05390c5de70e85a4e5ea76cd
Sam Johnson <x.smashmaster.x@gmail.com>
2014-09-24 21:35:29 -0600

    Added transformation matrices to RigidBody3D

--- src/com/samrj/devil/phys/RigidBody3D.java
+++ src/com/samrj/devil/phys/RigidBody3D.java
@@ -1,6 +1,7 @@
 package com.samrj.devil.phys;
 
 import com.samrj.devil.math.Matrix3f;
+import com.samrj.devil.math.Matrix4f;
 import com.samrj.devil.math.Quat4f;
 import com.samrj.devil.math.Vector3f;
 
@@ -18,6 +19,24 @@ public class RigidBody3D
     private final Quat4f orient = new Quat4f();
     private final Vector3f angMom = new Vector3f();
     private final Matrix3f moi = new Matrix3f();
+    
+    private final Matrix4f worldToLocal = new Matrix4f();
+    private final Matrix3f worldDirToLocal = new Matrix3f();
+    private final Matrix3f localDirToWorld = new Matrix3f();
+    
+    private void updateMatrices()
+    {
+        localDirToWorld.set(orient.toMatrix3f());
+        
+        Vector3f invPos = pos.cnegate();
+        Quat4f invOrient = orient.clone().invert();
+        Matrix3f invOrientMat3 = invOrient.toMatrix3f();
+        Matrix4f invOrientMat4 = invOrientMat3.toMatrix4f();
+        
+        worldDirToLocal.set(invOrientMat3);
+        worldToLocal.set(Matrix4f.translate(invPos));
+        worldToLocal.mult(invOrientMat4);
+    }
     
     public void applyImpulse(Vector3f impulse)
     {
