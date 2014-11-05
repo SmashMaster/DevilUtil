55f94fe78fb996ccc26eab477dfbf7b827094417
Sam Johnson <x.smashmaster.x@gmail.com>
2014-11-04 23:38:03 -0700

    Improved QuickGame

--- src/com/samrj/devil/util/QuickGame.java
+++ src/com/samrj/devil/util/QuickGame.java
@@ -5,6 +5,7 @@ import com.samrj.devil.ui.Input;
 import com.samrj.devil.ui.KeyEvent;
 import com.samrj.devil.ui.MouseEvent;
 import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.ContextAttribs;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.PixelFormat;
@@ -13,22 +14,28 @@ public abstract class QuickGame implements Element
 {
     private DisplayMode displayMode;
     private PixelFormat pixelFormat;
+    private ContextAttribs contextAttribs;
     private boolean running = false;
     
-    public QuickGame(DisplayMode dm, PixelFormat pf)
+    public QuickGame(DisplayMode dm, PixelFormat pf, ContextAttribs ca)
     {
+        if (dm == null) throw new NullPointerException();
+        if (pf == null) pf = new PixelFormat(0, 0, 0);
+        if (ca == null) ca = new ContextAttribs();
+        
         displayMode = dm;
         pixelFormat = pf;
+        contextAttribs = ca;
     }
     
     public QuickGame(int resX, int resY, PixelFormat pf)
     {
-        this(new DisplayMode(resX, resY), pf);
+        this(new DisplayMode(resX, resY), pf, null);
     }
     
     public QuickGame(int resX, int resY)
     {
-        this(resX, resY, new PixelFormat(0, 0, 0));
+        this(resX, resY, null);
     }
     
     public abstract void init();
@@ -36,12 +43,13 @@ public abstract class QuickGame implements Element
     @Override public abstract void in(KeyEvent in);
     public abstract void step(float dt);
     public abstract void render();
+    public abstract void destroy();
     
     public final void run() throws LWJGLException
     {
         Display.setDisplayMode(displayMode);
         Display.setVSyncEnabled(false);
-        Display.create(pixelFormat);
+        Display.create(pixelFormat, contextAttribs);
         
         init();
         
@@ -49,6 +57,11 @@ public abstract class QuickGame implements Element
         while (running)
         {
             Display.processMessages();
+            if (Display.isCloseRequested())
+            {
+                stop();
+                break;
+            }
             
             Input.step(this);
             step(1f/60f);
@@ -57,6 +70,10 @@ public abstract class QuickGame implements Element
             Display.update(false);
             Display.sync(60);
         }
+        
+        destroy();
+        
+        Display.destroy();
     }
     
     public final void stop()
