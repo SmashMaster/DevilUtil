2e0d1157187ef63004bfba8e089149f044c2eeab
Sam Johnson <x.smashmaster.x@gmail.com>
2015-02-24 04:45:36 -0700

    Triangulation - Swept line method
    Using a swept line algorithm instead of an incremental one. Seems to be
    easier to implement.

--- src/com/samrj/devil/geo2d/DelaunayTriangulation.java
+++ src/com/samrj/devil/geo2d/DelaunayTriangulation.java
@@ -1,8 +1,9 @@
 package com.samrj.devil.geo2d;
 
+import com.samrj.devil.math.Util;
 import com.samrj.devil.math.Vector2f;
+import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
 /**
@@ -12,129 +13,67 @@ import java.util.Set;
  */
 public class DelaunayTriangulation
 {
-    private Set<Edge> edges = new HashSet<>();
     private Set<Triangle> triangles = new HashSet<>();
     
-    public DelaunayTriangulation(Vector2f... pointArray)
+    public DelaunayTriangulation(Vector2f... points)
     {
-        Set<Vector2f> points = new HashSet<>(pointArray.length);
-        for (Vector2f point : pointArray)
-            points.add(Objects.requireNonNull(point));
+        Arrays.sort(points, (Vector2f v1, Vector2f v2) -> Util.signum(v1.x - v2.x));
         
-        AAB bounds = AAB.bounds(pointArray);
-        Vector2f size = bounds.size();
-        
-        //Create a triangle that contains all points in the set. 
-        Vector2f tempPointA, tempPointB, tempPointC;
+        Edge topLeadingEdge;
         {
-            tempPointA = new Vector2f(bounds.x0 - size.x*0.5f, bounds.y0 - size.y*0.5f);
-            tempPointB = new Vector2f(tempPointA.x, bounds.y1 + size.y*1.5f);
-            tempPointC = new Vector2f(bounds.x1 + size.x*1.5f, tempPointA.y);
-            
-            Triangle tempTri = new Triangle(tempPointA, tempPointB, tempPointC);
-            
-            tempTri.edgeAB = new Edge(tempPointA, tempPointB);
-            tempTri.edgeBC = new Edge(tempPointB, tempPointC);
-            tempTri.edgeCA = new Edge(tempPointC, tempPointA);
-            
-            tempTri.edgeAB.right = tempTri;
-            tempTri.edgeBC.right = tempTri;
-            tempTri.edgeCA.right = tempTri;
-            
-            triangles.add(tempTri);
-            edges.add(tempTri.edgeAB);
-            edges.add(tempTri.edgeBC);
-            edges.add(tempTri.edgeCA);
+            Vector2f p0 = points[0];
+            Vector2f p1 = points[1];
+
+            topLeadingEdge = p0.y > p1.y ? new Edge(p0, p1) : new Edge(p1, p0);
         }
         
-        //Incrementally build triangulation
-        for (Vector2f point : points)
+        for (int i=2; i<points.length; i++)
         {
-            Object containing = findContaining(point);
-            if (containing instanceof Triangle)
-            {
-                Triangle triangle = (Triangle)containing;
-                triangles.remove(triangle);
-                
-                Triangle triAB = new Triangle(triangle.vertA, triangle.vertB, point);
-                Triangle triBC = new Triangle(point, triangle.vertB, triangle.vertC);
-                Triangle triCA = new Triangle(triangle.vertA, point, triangle.vertC);
-                
-                //Update edge pointers
-                Edge edgePA = new Edge(point, triangle.vertA);
-                edgePA.left = triCA;
-                edgePA.right = triAB;
-                edges.add(edgePA);
-                
-                Edge edgePB = new Edge(point, triangle.vertB);
-                edgePB.left = triAB;
-                edgePB.right = triBC;
-                edges.add(edgePB);
-                
-                Edge edgePC = new Edge(point, triangle.vertC);
-                edgePC.left = triBC;
-                edgePC.right = triCA;
-                edges.add(edgePC);
-                
-                //Need to update triangle pointers
-                //Also finish the rest of the algorithm
-            }
-            else if (containing instanceof Edge)
-            {
-                Edge edge = (Edge)containing;
-            }
+            Edge edge = topLeadingEdge;
+            //Work our way down the top leading edge, generating triangles for
+            //all leading edges that face our point, and updating the leading
+            //edge as we go.
         }
-    }
-    
-    private Object findContaining(Vector2f point)
-    {
-        for (Triangle triangle : triangles)
-        {
-            Object out = triangle.side(point);
-            if (out != null) return out;
-        }
-        return null;
+        
+        //Fix all illegal triangles by flipping edges
     }
     
     private class Edge
     {
-        private Vector2f start, end;
-        private Triangle left, right;
+        private Vector2f a, b;
+        private Edge next;
         
-        private Edge(Vector2f start, Vector2f end)
+        private Edge(Vector2f a, Vector2f b)
         {
-            this.start = start; this.end = end;
+            this.a = a; this.b = b;
         }
     }
     
     private class Triangle
     {
-        private Vector2f vertA, vertB, vertC;
-        private Edge edgeAB, edgeBC, edgeCA;
-        private Triangle triAB, triBC, triCA;
+        private Vector2f a, b, c;
+        private Triangle ab, bc, ca;
+        private final Vector2f circumcenter = new Vector2f();
+        private float circumradiusSq;
         
         private Triangle(Vector2f a, Vector2f b, Vector2f c)
         {
-            vertA = a; vertB = b; vertC = c;
+            set(a, b, c);
         }
         
-        /**
-         * @return null for outside, edge for on edge, this for inside.
-         */
-        private Object side(Vector2f point)
+        private void set(Vector2f a, Vector2f b, Vector2f c)
         {
-            int abSide = new Line(vertA, vertB).side(point);
-            if (abSide < 0) return -1;
-            int bcSide = new Line(vertB, vertC).side(point);
-            if (bcSide < 0) return -1;
-            int caSide = new Line(vertC, vertA).side(point);
-            if (caSide < 0) return -1;
+            this.a = a; this.b = b; this.c = c;
             
-            if (abSide == 0) return edgeAB;
-            if (bcSide == 0) return edgeBC;
-            if (caSide == 0) return edgeCA;
+            float asq = a.x*a.x + a.y*a.y;
+            float bsq = b.x*b.x + b.y*b.y;
+            float csq = c.x*c.x + c.y*c.y;
             
-            return this;
+            circumcenter.x = asq*(b.y - c.y) + bsq*(c.y - a.y) + csq*(a.y - b.y);
+            circumcenter.y = asq*(c.x - b.x) + bsq*(a.x - c.x) + csq*(b.x - a.x);
+            circumcenter.div(2f*(a.x*(b.y - c.y) + b.x*(c.y - a.y) + c.x*(a.y - b.y)));
+            
+            circumradiusSq = a.squareDist(circumcenter);
         }
     }
 }
