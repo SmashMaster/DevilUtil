0402d54b39f9bdf240b17cd6e9662786b1ad13a5
Sam Johnson <sam@samrj.com>
2015-05-21 02:45:55 -0600

    Added float log base 2 method

--- src/com/samrj/devil/math/Util.java
+++ src/com/samrj/devil/math/Util.java
@@ -557,6 +557,35 @@ public class Util
     {
         return angleNrm(a.cnormalize(), b.cnormalize());
     }
+    
+    private static final int[] logTable = new int[256];
+    
+    static
+    {
+        logTable[0] = logTable[1] = 0;
+        for (int i=2; i<256; i++) logTable[i] = 1 + logTable[i/2];
+        logTable[0] = -1;
+    }
+    
+    /**
+     * Quickly calculates the floored log base 2 of the given float.
+     * 
+     * Undefined for negatives, zero, infinites and NaN.
+     */
+    public static final int log2(float f)
+    {
+        int x = Float.floatToIntBits(f);
+        int c = x >> 23;
+
+        if (c != 0) return c - 127; //Compute directly from exponent.
+        else //Subnormal, must compute from mantissa.
+        {
+            int t = x >> 16;
+            // Note that LogTable256 was defined earlier
+            if (t != 0) return logTable[t] - 133;
+            else return (x >> 8 != 0) ? logTable[t] - 141 : logTable[x] - 149;
+        }
+    }
     // </editor-fold>
     // <editor-fold defaultstate="collapsed" desc="Integer Math">
     public static int clamp(int i, int min, int max)
