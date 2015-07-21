b4c78fafff378d040c16c0a561cca11a5ebfc7c1
Sam Johnson <sam@samrj.com>
2015-07-20 22:50:18 -0600

    IK might be working
    But I'm never getting my hopes up again

--- src/com/samrj/devil/graphics/model/IKConstraint.java
+++ src/com/samrj/devil/graphics/model/IKConstraint.java
@@ -1,7 +1,6 @@
 package com.samrj.devil.graphics.model;
 
-import com.samrj.devil.math.Quat;
-import com.samrj.devil.math.Util;
+import com.samrj.devil.math.Mat4;
 import com.samrj.devil.math.Vec3;
 import java.io.DataInputStream;
 import java.io.IOException;
@@ -15,8 +14,22 @@ import java.io.IOException;
  */
 public class IKConstraint implements Solvable
 {
+    private static float getPoleAngle(Vec3 pole, Mat4 startMat, Vec3 start, Vec3 end)
+    {
+        Vec3 x = Vec3.sub(end, start).normalize();
+        Vec3 z = new Vec3(0, 0, 1).mult(startMat);
+        Vec3.cross(x, z, z);
+        Vec3 y = Vec3.cross(z, x);
+        
+        Vec3 localPole = Vec3.sub(pole, start);
+        float poleY = localPole.scalarProject(y);
+        float poleZ = localPole.scalarProject(z);
+        
+        return (float)Math.atan2(poleZ, poleY);
+    }
+    
     public final Bone parent, start, end, target, pole;
-    public final float poleAngle;
+    public final float poleAngle, initialPoleAngle;
     public final float startSqLen, endSqLen, startLen, endLen, length;
     
     public IKConstraint(DataInputStream in, Bone[] bones) throws IOException
@@ -37,15 +50,33 @@ public class IKConstraint implements Solvable
         startLen = (float)Math.sqrt(startSqLen);
         endLen = (float)Math.sqrt(endSqLen);
         length = startLen + endLen;
+        
+        initialPoleAngle = getPoleAngle(pole.head, start.baseMatrix, start.head, end.tail);
     }
     
     @Override
     public void solve()
     {
+        start.solveHeadPosition();
         
+        float curPoleAngle = getPoleAngle(pole.headFinal, start.baseMatrix, start.headFinal, target.headFinal);
+        float roll = curPoleAngle + poleAngle + initialPoleAngle;
         
+        float distSq = Vec3.squareDist(start.headFinal, target.headFinal);
+        float dist = (float)Math.sqrt(distSq);
+        float chordX = (distSq - endSqLen + startSqLen)/(dist*2.0f);
+        float chordY = (float)Math.sqrt(startSqLen - chordX*chordX);
+        float pitch = (float)Math.atan2(chordY, chordX);
         
-        start.solve();
-        end.solve();
+        start.reachTowards(target.headFinal);
+        start.rotation.rotate(new Vec3(1, 0, 0), roll);
+        start.rotation.rotate(new Vec3(0, 0, 1), pitch);
+        start.solveRotationMatrix();
+        start.solveTailPosition(); start.solveMatrix();
+        
+        end.solveHeadPosition();
+        end.reachTowards(target.headFinal);
+        end.solveRotationMatrix();
+        end.solveTailPosition(); end.solveMatrix();
     }
 }
