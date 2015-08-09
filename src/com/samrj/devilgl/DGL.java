b04bc9e7970ba4d23ff1be4a77ad32c11aba1d3d
Sam Johnson <sam@samrj.com>
2015-08-09 02:58:18 -0600

    Starting fresh again
    All of this work has already been moved to DevilUtil

---
+++ src/com/samrj/devilgl/DGL.java
@@ -0,0 +1,13 @@
+package com.samrj.devilgl;
+
+/**
+ * DevilGL. A state-based, object-oriented, forward compatible OpenGL wrapper,
+ * inspired by deprecated OpenGL.
+ * 
+ * @author Samuel Johnson (SmashMaster)
+ * @copyright 2015 Samuel Johnson
+ * @license https://github.com/SmashMaster/DevilGL/blob/master/LICENSE
+ */
+public class DGL
+{
+}
