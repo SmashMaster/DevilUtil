0cdae89bc6275eb73de9d3bea73e7ea1818654a5
Sam Johnson <x.smashmaster.x@gmail.com>
2015-09-11 06:53:32 -0600

    Fixed wrap mode on rectangle textures
    GL_REPEAT is not supported by texture rectangles

--- src/com/samrj/devil/gl/TextureRectangle.java
+++ src/com/samrj/devil/gl/TextureRectangle.java
@@ -1,6 +1,7 @@
 package com.samrj.devil.gl;
 
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
 import org.lwjgl.opengl.GL31;
 
 /**
@@ -17,8 +18,8 @@ public class TextureRectangle extends Texture2DAbstract
         super(GL31.GL_TEXTURE_RECTANGLE, GL31.GL_TEXTURE_BINDING_RECTANGLE);
         
         int oldID = tempBind();
-        parami(GL11.GL_TEXTURE_WRAP_S, GL11.GL_REPEAT);
-        parami(GL11.GL_TEXTURE_WRAP_T, GL11.GL_REPEAT);
+        parami(GL11.GL_TEXTURE_WRAP_S, GL12.GL_CLAMP_TO_EDGE);
+        parami(GL11.GL_TEXTURE_WRAP_T, GL12.GL_CLAMP_TO_EDGE);
         parami(GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
         parami(GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
         tempUnbind(oldID);
