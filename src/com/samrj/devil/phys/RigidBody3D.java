ccd4c2d975270a4469acafa60c0cf4cddb536592
Sam Johnson <sam@samrj.com>
2015-09-14 21:09:37 -0600

    Improved and documented RigidBody3D

--- src/com/samrj/devil/phys/RigidBody3D.java
+++ src/com/samrj/devil/phys/RigidBody3D.java
@@ -3,10 +3,13 @@ package com.samrj.devil.phys;
 import com.samrj.devil.math.Mat3;
 import com.samrj.devil.math.Mat4;
 import com.samrj.devil.math.Quat;
-import com.samrj.devil.math.Util;
 import com.samrj.devil.math.Vec3;
+import java.util.function.Function;
 
 /**
+ * Simple rigid body class. Handles force and torque integration, doesn't handle
+ * collisions, gravity, etc.
+ * 
  * @author Samuel Johnson (SmashMaster)
  * @copyright 2014 Samuel Johnson
  * @license https://github.com/SmashMaster/DevilUtil/blob/master/LICENSE
@@ -25,6 +28,15 @@ public class RigidBody3D
     public final Mat3 worldDirToLocal;
     public final Mat3 localDirToWorld;
     
+    private Function<RigidBody3D, Vec3> forceFunc, torqueFunc;
+    
+    /**
+     * Creates a new rigid body with the given properties. The moment of inertia
+     * is not multiplied by the mass, you must pre-multiply it.
+     * 
+     * @param mass The mass of this rigid body.
+     * @param moi The moment of inertia tensor of this rigid body.
+     */
     public RigidBody3D(float mass, MOI moi)
     {
         pos = new Vec3();
@@ -40,6 +52,11 @@ public class RigidBody3D
         localDirToWorld = Mat3.identity();
     }
     
+    /**
+     * Updates the matrices of this rigid body. Call when externally modifying
+     * the position or orientation of this body. Does not need to called when
+     * externally modifying velocity or mass.
+     */
     public void updateMatrices()
     {
         localDirToWorld.setRotation(orient);
@@ -52,13 +69,39 @@ public class RigidBody3D
         worldToLocal.mult(invOrientMat4);
     }
     
-    public Vec3 getForce()
+    /**
+     * Sets the function to use for determining the current net force on this
+     * rigid body.
+     * 
+     * @param func A function that takes this body as a parameter, and returns
+     *        the net force on this body.
+     */
+    public void setForceFunction(Function<RigidBody3D, Vec3> func)
     {
+        forceFunc = func;
+    }
+    
+    /**
+     * Sets the function to use for determining the current net torque on this
+     * rigid body.
+     * 
+     * @param func A function that takes this body as a parameter, and returns
+     *        the net torque on this body.
+     */
+    public void setTorqueFunction(Function<RigidBody3D, Vec3> func)
+    {
+        torqueFunc = func;
+    }
+    
+    private Vec3 force()
+    {
+        if (forceFunc != null) return forceFunc.apply(this);
         return new Vec3();
     }
     
-    public Vec3 getTorque()
+    private Vec3 torque()
     {
+        if (torqueFunc != null) return torqueFunc.apply(this);
         return new Vec3();
     }
     
@@ -67,12 +110,23 @@ public class RigidBody3D
         return Mat3.mult(localDirToWorld, moi.inv).mult(worldDirToLocal);
     }
     
+    /**
+     * Calculates the acceleration on this body and integrates it over the given
+     * time. Integration will be symplectic as long as the force is not
+     * dependant on the velocity (as with air resistance). Uses a modified form
+     * of Verlet integration.
+     * 
+     * The properties of this integration lend themselves well to cylic systems
+     * such as orbits and springs.
+     * 
+     * @param dt The time step to integrate over.
+     */
     public void step(float dt)
     {
         float hdt = dt*.5f;
         
-        Vec3 acc0 = getForce().div(mass); //Initial acceleration
-        Vec3 angAcc0 = getTorque().mult(rotatedInvMOI());
+        Vec3 acc0 = force().div(mass); //Initial acceleration
+        Vec3 angAcc0 = torque().mult(rotatedInvMOI());
         
         Vec3 velh = Vec3.mult(acc0, hdt).add(vel); //Half-step velocity
         Vec3 angVelh = Vec3.mult(angAcc0, hdt).add(angVel);
@@ -90,8 +144,8 @@ public class RigidBody3D
         
         updateMatrices();
         
-        Vec3 acc1 = getForce().div(mass); //Final acceleration
-        Vec3 angAcc1 = getTorque().mult(rotatedInvMOI());
+        Vec3 acc1 = force().div(mass); //Final acceleration
+        Vec3 angAcc1 = torque().mult(rotatedInvMOI());
         
         vel.set(acc1).mult(hdt).add(velh); //Final velocity
         angVel.set(angAcc1).mult(hdt).add(angVelh);
@@ -99,27 +153,57 @@ public class RigidBody3D
         updateMatrices();
     }
     
+    /**
+     * Applies a one-time linear impulse to the center of mass of this body.
+     * 
+     * @param impulse The impulse to apply.
+     */
     public void applyImpulse(Vec3 impulse)
     {
         vel.add(Vec3.div(impulse, mass));
     }
     
+    /**
+     * Applies a one-time impulse to the given position on this body, in local
+     * coordinates.
+     * 
+     * @param impulse The impulse to apply.
+     * @param localPosition The local position to apply the impulse to.
+     */
     public void applyImpulse(Vec3 impulse, Vec3 localPosition)
     {
         applyImpulse(impulse);
         applyAngularImpulse(Vec3.cross(impulse, localPosition));
     }
     
+    /**
+     * Applies a one-time angular impulse to this body.
+     * 
+     * @param angImp The angular impulse to apply.
+     */
     public void applyAngularImpulse(Vec3 angImp)
     {
         angVel.add(Vec3.mult(angImp, moi.inv));
     }
     
+    /**
+     * Returns the velocity of this body at the given point. The point is in
+     * world space, but is relative to the position of this body.
+     * 
+     * @param point The point to find the velocity at.
+     * @return The velocity at the given point.
+     */
     public Vec3 getPointVelocity(Vec3 point)
     {
         return Vec3.cross(angVel, point).add(vel);
     }
     
+    /**
+     * Returns the velocity of this body at the given local point.
+     * 
+     * @param localPoint The local point to find the velocity at.
+     * @return The velocity at the given point.
+     */
     public Vec3 getLocalPointVelocity(Vec3 localPoint)
     {
         return getPointVelocity(Vec3.mult(localPoint, localDirToWorld));
