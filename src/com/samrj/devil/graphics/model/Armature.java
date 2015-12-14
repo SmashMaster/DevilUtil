4a73d38849415c18bba8f750a1cf6419b81a9110
Sam Johnson <x.smashmaster.x@gmail.com>
2015-12-14 05:47:15 -0700

    Implementing animations
    Poses should be more generally useful.

--- src/com/samrj/devil/graphics/model/Armature.java
+++ src/com/samrj/devil/graphics/model/Armature.java
@@ -6,13 +6,16 @@ import com.samrj.devil.math.topo.DAG;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Armature implements DataBlock
 {
     public final String name;
     public final Bone[] bones;
-    private final List<Bone> topo;
+    private final Map<String, Bone> nameMap;
+    private final List<Bone> topoSorted;
     
     public final Memory matrixBlock;
     public final ByteBuffer matrixData;
@@ -23,6 +26,9 @@ public class Armature implements DataBlock
         bones = IOUtil.arrayFromStream(in, Bone.class, Bone::new);
         for (Bone bone : bones) bone.populate(bones);
         
+        nameMap = new HashMap<>();
+        for (Bone bone : bones) nameMap.put(bone.name, bone);
+        
         //Do a topological sort so that we can simply update matrices in-order.
         DAG<Bone> dag = new DAG<>();
         for (Bone bone : bones) dag.add(bone);
@@ -32,16 +38,21 @@ public class Armature implements DataBlock
             if (parent == null) continue;
             dag.addEdge(parent, bone);
         }
-        topo = dag.sort();
+        topoSorted = dag.sort();
         
         matrixBlock = new Memory(bones.length*16*4);
         matrixData = matrixBlock.buffer;
     }
     
+    public Bone getBone(String name)
+    {
+        return nameMap.get(name);
+    }
+    
     public void updatePoseMatrices()
     {
         matrixData.rewind();
-        for (Bone bone : topo)
+        for (Bone bone : topoSorted)
         {
             bone.updatePoseMatrix();
             bone.poseMatrix.write(matrixData);
--- src/com/samrj/devil/graphics/model/FCurve.java
+++ src/com/samrj/devil/graphics/model/FCurve.java
@@ -4,13 +4,171 @@ import com.samrj.devil.io.IOUtil;
 import com.samrj.devil.math.Vec2;
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.TreeMap;
 
 public class FCurve
 {
-    public String boneName;
-    public int propertyID;
-    public int arrayIndex;
-    public Keyframe[] keyframes;
+    public static enum Interpolation
+    {
+        CONSTANT, LINEAR, BEZIER;
+    }
+    
+    /**
+     * The total length of the handles is not allowed to be more
+     * than the horizontal distance between left and right.
+     */
+    private static void validate(Keyframe keyLeft, Keyframe keyRight)
+    {
+        float width = keyRight.co.x - keyLeft.co.x;
+        
+        Vec2 dHandleLeft = Vec2.sub(keyLeft.right, keyLeft.co);
+        Vec2 dHandleRight = Vec2.sub(keyRight.left, keyRight.co);
+        
+        float leftLength = dHandleLeft.length();
+        float rightLength = dHandleRight.length();
+        float totalLength = leftLength + rightLength;
+        
+        if (totalLength > width)
+        {
+            float adjFactor = width/totalLength;
+            dHandleLeft.mult(adjFactor);
+            dHandleRight.mult(adjFactor);
+            
+            keyLeft.right.set(keyLeft.co).add(dHandleLeft);
+            keyRight.left.set(keyRight.co).add(dHandleRight);
+        }
+    }
+    
+    private static List<Float> bezierT(float x0, float x1, float x2, float x3, float x)
+    {
+        float c3 = -x0 + 3.0f*(x1 - x2) + x3;
+        float c2 = 3.0f*(x0 - 2.0f*x1 + x2);
+        float c1 = 3.0f*(-x0 + x1);
+        float c0 = x0 - x;
+
+        List<Float> out = new ArrayList<>(3);
+
+        if (c3 != 0.0f)
+        {
+            float a = c2/c3;
+            float b = c1/c3;
+            float c = c0/c3;
+            a = a/3;
+
+            float p = b/3.0f - a*a;
+            float q = (2*a*a*a - a*b + c)/2.0f;
+            float d = q*q + p*p*p;
+
+            if (d > 0.0f)
+            {
+                float t = (float)Math.sqrt(d);
+                float o = (float)Math.cbrt(-q + t) + (float)Math.cbrt(-q - t) - a;
+                if (o >= 0.0f && o <= 1.0f) out.add(o);
+            }
+            else if (d == 0.0f)
+            {
+                float t = (float)Math.cbrt(-q);
+                float o = 2*t - a;
+                if (o >= 0.0f && o <= 1.0f) out.add(o);
+
+                o = -t - a;
+                if (o >= 0.0f && o <= 1.0f) out.add(o);
+            }
+            else
+            {
+                //Oh god please why
+                float phi = (float)Math.acos(-q / (float)Math.sqrt(-(p*p*p)));
+                float t = (float)Math.sqrt(-p);
+                p = (float)Math.cos(phi/3.0f);
+                q = (float)Math.sqrt(3.0f - 3.0f*p*p);
+                float o = 2.0f*t*p - a;
+                if (o >= 0.0f && o <= 1.0f) out.add(o);
+
+                o = -t*(p + q) - a;
+                if (o >= 0.0f && o <= 1.0f) out.add(o);
+
+                o = -t*(p - q) - a;
+                if (o >= 0.0f && o <= 1.0f) out.add(o);
+            }
+        }
+        else
+        {
+            float a = c2;
+            float b = c1;
+            float c = c0;
+
+            if (a != 0.0f)
+            {
+                float p = b*b - 4.0f*a*c; //Discriminant
+
+                if (p > 0.0f)
+                {
+                    p = (float)Math.sqrt(p);
+                    float o = (-b - p)/(2.0f*a);
+                    if (o >= 0.0f && o <= 1.0f) out.add(o);
+
+                    o = (-b + p)/(2.0f*a);
+                    if (o >= 0.0f && o <= 1.0f) out.add(o);
+                }
+                else if (p == 0.0f)
+                {
+                    float o = -b/(2.0f*a);
+                    if (o >= 0.0f && o <= 1.0f) out.add(o);
+                }
+            }
+            else if (b != 0.0f)
+            {
+                float o = -c/b;
+                if (o >= 0.0f && o <= 1.0f) out.add(o);
+            }
+            else if (c == 0.0f) out.add(0.0f);
+        }
+
+        return out;
+    }
+    
+    private static float bezierY(float y0, float y1, float y2, float y3, float t)
+    {
+        float omt = 1.0f - t;
+        return omt*(omt*omt*y0 + 3.0f*t*(omt*y1 + t*y2)) + t*t*t*y3;
+    }
+    
+    private static float bezier(Vec2 p0, Vec2 p1, Vec2 p2, Vec2 p3, float x)
+    {
+        List<Float> tSolutions = bezierT(p0.x, p1.x, p2.x, p3.x, x);
+        float t;
+        if (tSolutions.isEmpty()) t = (x - p0.x)/(p3.x - p0.x); //Revert to lerp
+        else t = tSolutions.get(0);
+        
+        return bezierY(p0.y, p1.y, p2.y, p3.y, t);
+    }
+    
+    /**
+     * See the following source code for a correct implementation of bezier FCurve keyframes:
+     * 
+     * https://svn.blender.org/svnroot/bf-blender/trunk/blender/source/blender/blenkernel/intern/fcurve.c
+     */
+    private static float evaluate(Keyframe left, Keyframe right, float time)
+    {
+        if (time <= left.co.x || left.interpolation == Interpolation.CONSTANT)
+            return left.co.y;
+        if (time >= right.co.x) return right.co.y;
+        
+        if (left.interpolation == Interpolation.LINEAR)
+            return left.co.y + (right.co.y - left.co.y)*(time - left.co.x)/(right.co.x - left.co.x);
+        
+        return bezier(left.co, left.right, right.left, right.co, time);
+    }
+    
+    public final String boneName;
+    public final int propertyID;
+    public final int arrayIndex;
+    public final Keyframe[] keyframes;
+    public final float minX, maxX;
+    private final TreeMap<Float, Integer> keyInds;
     
     FCurve(DataInputStream in) throws IOException
     {
@@ -19,16 +177,61 @@ public class FCurve
         boneName = hasBone ? IOUtil.readPaddedUTF(in) : null;
         arrayIndex = in.readInt();
         keyframes = IOUtil.arrayFromStream(in, Keyframe.class, Keyframe::new);
+        
+        float min = Float.POSITIVE_INFINITY, max = Float.NEGATIVE_INFINITY;
+        keyInds = new TreeMap<>();
+        for (int i=0; i<keyframes.length; i++)
+        {
+            keyframes[i] = new Keyframe(in);
+            float x = keyframes[i].co.x;
+            if (x < min) min = x;
+            if (x > max) max = x;
+            keyInds.put(x, i);
+        }
+        minX = min; maxX = max;
+        
+        for (int i=0; i<keyframes.length - 1; i++)
+            validate(keyframes[i], keyframes[i + 1]);
+    }
+    
+    public float evaluate(float time)
+    {
+        Entry<Float, Integer> e0 = keyInds.floorEntry(time);
+        if (e0 == null) return keyframes[0].co.y; //Before first
+        
+        int i0 = e0.getValue();
+        Keyframe k0 = keyframes[i0];
+        if (i0 == keyframes.length - 1) return k0.co.y; //After last
+        
+        Keyframe k1 = keyframes[i0 + 1];
+        return evaluate(k0, k1, time);
+    }
+    
+    public void apply(Armature armature, float time)
+    {
+        Bone bone = armature.getBone(boneName);
+        if (bone == null) return;
+        float value = evaluate(time);
+        throw new UnsupportedOperationException();
+        //Set bone/pose property to evaluation
+        //bone.set(property, propIndex, value);
     }
     
     public class Keyframe
     {
-        public final int interpolation;
+        public final Interpolation interpolation;
         public final Vec2 co, left, right;
         
         Keyframe(DataInputStream in) throws IOException
         {
-            interpolation = in.readInt();
+            switch (in.readInt())
+            {
+                case 0: interpolation = Interpolation.CONSTANT; break;
+                case 1: interpolation = Interpolation.LINEAR; break;
+                case 2: interpolation = Interpolation.BEZIER; break;
+                default: interpolation = Interpolation.LINEAR;
+            }
+            
             co = new Vec2(in);
             left = new Vec2(in);
             right = new Vec2(in);
