3595b77792a4f82faacd9c0d7e40cacb57ab07f9
Sam Johnson <sam@samrj.com>
2016-01-17 21:54:12 -0700

    Split FPSPlayer into new class Actor

---
+++ src/com/samrj/devil/phys/Actor.java
@@ -0,0 +1,221 @@
+package com.samrj.devil.phys;
+
+import com.samrj.devil.geo3d.Ellipsoid;
+import com.samrj.devil.geo3d.Geo3DUtil;
+import com.samrj.devil.geo3d.GeoMesh;
+import com.samrj.devil.geo3d.SweepResult;
+import com.samrj.devil.math.Util;
+import com.samrj.devil.math.Vec3;
+import com.samrj.devil.phys.Actor.Settings;
+
+/**
+ * Basic class handling collision and movement.
+ * 
+ * @author Samuel Johnson (SmashMaster)
+ * @param <SETTINGS_TYPE> The type of settings this player uses.
+ * @copyright 2015 Samuel Johnson
+ * @license https://github.com/SmashMaster/DevilUtil/blob/master/LICENSE
+ */
+public abstract class Actor<SETTINGS_TYPE extends Settings>
+{
+    public final Vec3 pos, vel = new Vec3();
+    public final Vec3 moveDir = new Vec3();
+    public boolean noclip, noclipTurbo;
+    
+    protected final SETTINGS_TYPE settings;
+    protected final Ellipsoid shape = new Ellipsoid();
+    protected final GeoMesh level;
+    protected final Vec3 displacement = new Vec3();
+    protected Vec3 ground;
+    protected int groundMaterial;
+    protected boolean applyGravity;
+    
+    public Runnable jumpCallback, landCallback;
+    
+    /**
+     * Creates a new FPSPlayer using the given parameters, keyboard, camera, and
+     * level.
+     * 
+     * @param settings The FPSPlayer parameters to use.
+     * @param level The level to collide with.
+     */
+    public Actor(SETTINGS_TYPE settings, GeoMesh level)
+    {
+        if (settings == null || level == null) throw new NullPointerException();
+        
+        settings.calcValues();
+        this.settings = settings;
+        shape.radii.set(settings.width, settings.height, settings.width).mult(0.5f);
+        pos = shape.pos;
+        this.level = level;
+    }
+    
+    public abstract void onMouseMoved(float x, float y, float dx, float dy);
+    
+    
+    /**
+     * Makes the player jump. May only jump when standing on something.
+     */
+    public void jump()
+    {
+        if (ground == null) return;
+        vel.y = settings.jumpSpeed;
+        ground = null;
+        if (jumpCallback != null) jumpCallback.run();
+    }
+    
+    /**
+     * @return Whether or not the player is currently on walkable ground.
+     */
+    public boolean onGround()
+    {
+        return ground != null;
+    }
+    
+    /**
+     * Sets the ground of this player to a virtual, flat surface.
+     */
+    public void setFlatGround()
+    {
+        ground = new Vec3(0.0f, 1.0f, 0.0f);
+    }
+    
+    /**
+     * @return The material index of the ground last walked on.
+     */
+    public int groundMaterial()
+    {
+        return groundMaterial;
+    }
+    
+    protected void applyAcc(Vec3 desiredVel, float acc)
+    {
+        Vec3 dv = Vec3.sub(desiredVel, vel);
+        float dvLen = dv.length();
+        
+        if (dvLen > acc) vel.madd(dv, acc/dvLen);
+        else vel.set(desiredVel);
+    }
+    
+    /**
+     * Steps the player's simulation forward by the given time-step.
+     * 
+     * @param dt The time to step forward by.
+     */
+    public void step(float dt)
+    {
+        Vec3 avgVel = new Vec3(vel);
+        
+        if (noclip) //Noclipping
+        {
+            if (!vel.isZero(0.0f))
+            {
+                vel.normalize();
+                vel.mult(noclipTurbo ? settings.noclipTurboSpeed : settings.noclipSpeed);
+            }
+        }
+        else //Walking/falling
+        {
+            boolean wantToMove = !moveDir.isZero(0.0f);
+
+            if (ground != null) //Walking
+            {
+                if (wantToMove)
+                {
+                    moveDir.y = -ground.dot(moveDir)/ground.y;
+                    moveDir.normalize().mult(settings.maxSpeed);
+                }
+
+                //Lock to ground
+                Geo3DUtil.restrain(vel, Vec3.negate(ground));
+                applyAcc(moveDir, settings.acceleration*dt);
+            }
+            else //Falling
+            {
+                if (wantToMove)
+                {
+                    moveDir.normalize().mult(settings.maxSpeed);
+                    moveDir.y = vel.y;
+                    applyAcc(moveDir, settings.airAcceleration*dt);
+                }
+            }
+            
+            if (applyGravity) vel.y -= settings.gravity*dt;
+        }
+        
+        avgVel.add(vel).mult(0.5f);
+        Vec3.mult(avgVel, dt, displacement);
+        pos.add(displacement);
+        
+        applyGravity = true;
+        
+        if (!noclip)
+        {
+            boolean startGround = ground != null;
+            
+            //Find the ground
+            if (startGround)
+            {
+                ground = null;
+                float oldY = pos.y;
+                pos.y += settings.stepHeight;
+                Vec3 step = new Vec3(0.0f, -2.0f*settings.stepHeight, 0.0f);
+                SweepResult sweep = level.sweepUnsorted(shape, step)
+                        .filter(e -> e.normal.y >= settings.groundNormalMinY)
+                        .reduce((a, b) -> a.time < b.time ? a : b)
+                        .orElse(null);
+
+                pos.y = oldY;
+                if (sweep != null)
+                {
+                    ground = sweep.normal;
+                    Object object = sweep.object;
+                    if (object instanceof GeoMesh.Face) groundMaterial = ((GeoMesh.Face)object).material;
+                    applyGravity = (sweep.time - 0.5f)*2.0f > settings.groundThreshold;
+                }
+            }
+
+            //Clip against the level
+            level.intersectUnsorted(shape).forEach(isect ->
+            {
+                pos.add(Vec3.sub(isect.point, isect.surface));
+                Geo3DUtil.restrain(vel, isect.normal);
+                
+                if (isect.normal.y < settings.groundNormalMinY) return;
+                if (ground == null || isect.normal.y > ground.y)
+                {
+                    ground = isect.normal;
+                    Object object = isect.object;
+                    if (object instanceof GeoMesh.Face) groundMaterial = ((GeoMesh.Face)object).material;
+                }
+            });
+            
+            //Check for landing
+            if (landCallback != null && !startGround && ground != null)
+                landCallback.run();
+        }
+    }
+    
+    public static class Settings
+    {
+        public float gravity = 9.80665f;
+        public float width = 1.0f;
+        public float height = 1.75f;
+        public float stepHeight = 0.25f;
+        public float groundMaxIncline = Util.toRadians(46.0f);
+        public float groundThreshold = 1.0f/64.0f;
+        public float maxSpeed = 3.0f;
+        public float acceleration = 16.0f;
+        public float airAcceleration = 4.0f;
+        public float jumpSpeed = 4.0f;
+        public float noclipSpeed = 4.0f;
+        public float noclipTurboSpeed = 12.0f;
+        
+        protected float groundNormalMinY;
+        
+        protected void calcValues()
+        {
+            groundNormalMinY = (float)Math.cos(groundMaxIncline);
+        }
+    }
+}
--- src/com/samrj/devil/phys/FPSPlayer.java
+++ src/com/samrj/devil/phys/FPSPlayer.java
@@ -1,13 +1,9 @@
 package com.samrj.devil.phys;
 
 import com.samrj.devil.game.Keyboard;
-import com.samrj.devil.geo3d.Ellipsoid;
-import com.samrj.devil.geo3d.Geo3DUtil;
 import com.samrj.devil.geo3d.GeoMesh;
-import com.samrj.devil.geo3d.GeoMesh.Face;
-import com.samrj.devil.geo3d.SweepResult;
-import com.samrj.devil.math.Util;
 import com.samrj.devil.math.Vec3;
+import com.samrj.devil.phys.FPSPlayer.Settings;
 import com.samrj.devil.util.FPSCamera;
 import org.lwjgl.glfw.GLFW;
 
@@ -18,22 +14,13 @@ import org.lwjgl.glfw.GLFW;
  * @copyright 2014 Samuel Johnson
  * @license https://github.com/SmashMaster/DevilUtil/blob/master/LICENSE
  */
-public class FPSPlayer
+public class FPSPlayer extends Actor<Settings>
 {
-    public final Vec3 pos, vel = new Vec3();
-    public boolean noclip;
+    protected final Keyboard keyboard;
+    protected final FPSCamera camera;
     
-    private final Settings settings;
-    private final Keyboard keyboard;
-    private final Ellipsoid shape;
-    private final FPSCamera camera;
-    private final GeoMesh level;
-    private Vec3 ground;
-    private int groundMaterial;
-    private boolean applyGravity;
+    public Runnable stepCallback;
     private float stepAccum;
-    
-    public Runnable stepCallback, jumpCallback, landCallback;
     
     /**
      * Creates a new FPSPlayer using the given parameters, keyboard, camera, and
@@ -46,17 +33,14 @@ public class FPSPlayer
      */
     public FPSPlayer(Settings settings, Keyboard keyboard, FPSCamera camera, GeoMesh level)
     {
+        super(settings, level);
         if (settings == null || keyboard == null || camera == null || level == null)
             throw new NullPointerException();
         
         settings.calcValues();
-        this.settings = settings;
         this.keyboard = keyboard;
-        shape = new Ellipsoid();
         shape.radii.set(settings.width, settings.height, settings.width).mult(0.5f);
-        pos = shape.pos;
         this.camera = camera;
-        this.level = level;
     }
     
     /**
@@ -76,36 +60,10 @@ public class FPSPlayer
      * Call to move the player's view around. You may alternatively call
      * camera.onMouseMoved().
      */
+    @Override
     public void onMouseMoved(float x, float y, float dx, float dy)
     {
         camera.onMouseMoved(x, y, dx, dy);
-    }
-    
-    /**
-     * Makes the player jump. May only jump when standing on something.
-     */
-    public void jump()
-    {
-        if (ground == null) return;
-        vel.y = settings.jumpSpeed;
-        ground = null;
-        if (jumpCallback != null) jumpCallback.run();
-    }
-    
-    /**
-     * @return Whether or not the player is currently on walkable ground.
-     */
-    public boolean onGround()
-    {
-        return ground != null;
-    }
-    
-    /**
-     * Sets the ground of this player to a virtual, flat surface.
-     */
-    public void setFlatGround()
-    {
-        ground = new Vec3(0.0f, 1.0f, 0.0f);
     }
     
     /**
@@ -120,31 +78,12 @@ public class FPSPlayer
     }
     
     /**
-     * @return The material index of the ground last walked on.
-     */
-    public int groundMaterial()
-    {
-        return groundMaterial;
-    }
-    
-    private void applyAcc(Vec3 desiredVel, float acc)
-    {
-        Vec3 dv = Vec3.sub(desiredVel, vel);
-        float dvLen = dv.length();
-        
-        if (dvLen > acc) vel.madd(dv, acc/dvLen);
-        else vel.set(desiredVel);
-    }
-    
-    /**
      * Steps the player's simulation forward by the given time-step.
      * 
      * @param dt The time to step forward by.
      */
     public void step(float dt)
     {
-        Vec3 avgVel = new Vec3(vel);
-        
         if (noclip) //Noclipping
         {
             vel.set();
@@ -154,13 +93,7 @@ public class FPSPlayer
             if (keyboard.isKeyDown(GLFW.GLFW_KEY_A)) vel.sub(camera.right);
             if (keyboard.isKeyDown(GLFW.GLFW_KEY_SPACE)) vel.y += 1.0f;
             if (keyboard.isKeyDown(GLFW.GLFW_KEY_LEFT_CONTROL)) vel.y -= 1.0f;
-
-            if (!vel.isZero(0.0f))
-            {
-                vel.normalize();
-                boolean turbo = keyboard.isKeyDown(GLFW.GLFW_KEY_LEFT_SHIFT);
-                vel.mult(turbo ? settings.noclipTurboSpeed : settings.noclipSpeed);
-            }
+            noclipTurbo = keyboard.isKeyDown(GLFW.GLFW_KEY_LEFT_SHIFT);
         }
         else //Walking/falling
         {
@@ -170,85 +103,17 @@ public class FPSPlayer
             Vec3 flatForward = new Vec3(-sin, 0.0f, -cos);
             Vec3 flatRight   = new Vec3(cos, 0.0f, -sin);
 
-            Vec3 moveDir = new Vec3();
+            moveDir.set();
             if (keyboard.isKeyDown(GLFW.GLFW_KEY_S)) moveDir.sub(flatForward);
             if (keyboard.isKeyDown(GLFW.GLFW_KEY_W)) moveDir.add(flatForward);
             if (keyboard.isKeyDown(GLFW.GLFW_KEY_A)) moveDir.sub(flatRight);
             if (keyboard.isKeyDown(GLFW.GLFW_KEY_D)) moveDir.add(flatRight);
-            boolean wantToMove = !moveDir.isZero(0.0f);
-
-            if (ground != null) //Walking
-            {
-                if (wantToMove)
-                {
-                    moveDir.y = -ground.dot(moveDir)/ground.y;
-                    moveDir.normalize().mult(settings.maxSpeed);
-                }
-
-                //Lock to ground
-                Geo3DUtil.restrain(vel, Vec3.negate(ground));
-                applyAcc(moveDir, settings.acceleration*dt);
-            }
-            else //Falling
-            {
-                if (wantToMove)
-                {
-                    moveDir.normalize().mult(settings.maxSpeed);
-                    moveDir.y = vel.y;
-                    applyAcc(moveDir, settings.airAcceleration*dt);
-                }
-            }
-            
-            if (applyGravity) vel.y -= settings.gravity*dt;
         }
         
-        avgVel.add(vel).mult(0.5f);
-        Vec3 displacement = Vec3.mult(avgVel, dt);
-        pos.add(displacement);
-        
-        applyGravity = true;
+        super.step(dt);
         
         if (!noclip)
         {
-            boolean startGround = ground != null;
-            
-            //Find the ground
-            if (startGround)
-            {
-                ground = null;
-                float oldY = pos.y;
-                pos.y += settings.stepHeight;
-                Vec3 step = new Vec3(0.0f, -2.0f*settings.stepHeight, 0.0f);
-                SweepResult sweep = level.sweepUnsorted(shape, step)
-                        .filter(e -> e.normal.y >= settings.groundNormalMinY)
-                        .reduce((a, b) -> a.time < b.time ? a : b)
-                        .orElse(null);
-
-                pos.y = oldY;
-                if (sweep != null)
-                {
-                    ground = sweep.normal;
-                    Object object = sweep.object;
-                    if (object instanceof Face) groundMaterial = ((Face)object).material;
-                    applyGravity = (sweep.time - 0.5f)*2.0f > settings.groundThreshold;
-                }
-            }
-
-            //Clip against the level
-            level.intersectUnsorted(shape).forEach(isect ->
-            {
-                pos.add(Vec3.sub(isect.point, isect.surface));
-                Geo3DUtil.restrain(vel, isect.normal);
-                
-                if (isect.normal.y < settings.groundNormalMinY) return;
-                if (ground == null || isect.normal.y > ground.y)
-                {
-                    ground = isect.normal;
-                    Object object = isect.object;
-                    if (object instanceof Face) groundMaterial = ((Face)object).material;
-                }
-            });
-            
             //Check for footsteps
             if (stepCallback != null && ground != null)
             {
@@ -262,10 +127,6 @@ public class FPSPlayer
                 }
             }
             else stepAccum = 0.0f;
-            
-            //Check for landing
-            if (landCallback != null && !startGround && ground != null)
-                landCallback.run();
         }
         
         camera.pos.set(pos);
@@ -277,27 +138,15 @@ public class FPSPlayer
      * A container for all of the parameters needed to define a player. Has good
      * defaults for each value.
      */
-    public static class Settings
+    public static class Settings extends Actor.Settings
     {
-        public float gravity = 9.80665f;
-        public float width = 1.0f;
-        public float height = 1.75f;
-        public float stepHeight = 0.25f;
-        public float groundMaxIncline = Util.toRadians(46.0f);
-        public float groundThreshold = 1.0f/64.0f;
-        public float maxSpeed = 3.0f;
-        public float acceleration = 16.0f;
-        public float airAcceleration = 4.0f;
-        public float jumpSpeed = 4.0f;
-        public float noclipSpeed = 4.0f;
-        public float noclipTurboSpeed = 12.0f;
         public float cameraHeight = 1.640625f;
         public float strideLength = 1.25f;
         
-        private float groundNormalMinY;
-        private float cameraOffset;
+        protected float cameraOffset;
         
-        private void calcValues()
+        @Override
+        protected void calcValues()
         {
             groundNormalMinY = (float)Math.cos(groundMaxIncline);
             cameraOffset = cameraHeight - height*0.5f;
