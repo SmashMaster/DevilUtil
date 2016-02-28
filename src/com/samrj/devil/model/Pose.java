b1e49fac975b3e3d104ce5b27ba713b227400330
Sam Johnson <sam@samrj.com>
2016-02-28 03:21:13 -0700

    Working on pose refactoring

--- src/com/samrj/devil/model/Pose.java
+++ src/com/samrj/devil/model/Pose.java
@@ -1,14 +1,15 @@
 package com.samrj.devil.model;
 
+import com.samrj.devil.io.IOUtil;
 import com.samrj.devil.model.Transform.Property;
 import java.io.DataInputStream;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * Class for storing and manipulating bone pose information.
+ * 
  * @author Samuel Johnson (SmashMaster)
  * @copyright 2015 Samuel Johnson
  * @license https://github.com/SmashMaster/DevilUtil/blob/master/LICENSE
@@ -16,7 +17,6 @@ import java.util.Map;
 public class Pose
 {
     private final Map<String, PoseBone> bones;
-    private Armature armature;
     
     Pose(DataInputStream in) throws IOException
     {
@@ -29,58 +29,185 @@ public class Pose
         }
     }
     
+    /**
+     * Creates a new deep copy of the given pose.
+     * 
+     * @param pose A pose to copy.
+     */
     public Pose(Pose pose)
     {
         bones = new HashMap<>(pose.bones.size());
         for (PoseBone bone : pose.bones.values())
             bones.put(bone.name, new PoseBone(bone));
-        
-        populate(pose.armature);
     }
     
-    public Armature getArmature()
+    /**
+     * Creates a new blank pose.
+     */
+    public Pose()
     {
-        return armature;
+        bones = new HashMap<>();
     }
     
-    public Collection<PoseBone> getBones()
-    {
-        return Collections.unmodifiableCollection(bones.values());
-    }
-    
-    void populate(Armature armature)
-    {
-        this.armature = armature;
-        for (PoseBone bone : bones.values()) bone.populate(armature, this);
-    }
-    
-    public void setBoneProperty(String name, Property property, int index, float value)
-    {
-        PoseBone bone = bones.get(name);
-        if (bone == null) throw new IllegalArgumentException("No such bone '" + name + "'");
-        bone.transform.setProperty(property, index, value);
-    }
-    
+    /**
+     * Returns the bone with the given name, or null if there is no such bone.
+     * 
+     * @param name
+     * @return 
+     */
     public PoseBone getBone(String name)
     {
         return bones.get(name);
     }
     
+    /**
+     * Sets a specific property of a bone.
+     * 
+     * @param name The name of the bone to change.
+     * @param property The property to change.
+     * @param index The index of the value to change.
+     * @param value Any float.
+     * @return This pose.
+     */
+    public Pose setProperty(String name, Property property, int index, float value)
+    {
+        PoseBone bone = bones.get(name);
+        if (bone == null) bones.put(name, bone = new PoseBone(name));
+        bone.transform.setProperty(property, index, value);
+        return this;
+    }
+    
+    /**
+     * Sets this to a blank pose.
+     * 
+     * @return This pose.
+     */
     public Pose clear()
     {
         bones.clear();
         return this;
     }
     
-    public Pose mix(Pose pose, float t)
+    /**
+     * Removes any bones not shared between this and the given pose.
+     * 
+     * @param pose The pose to mask with.
+     * @return This pose.
+     */
+    public Pose intersect(Pose pose)
     {
-        for (PoseBone target : pose.bones.values())
+        return this;
+    }
+    
+    /**
+     * Removes any bone shared between this and the given pose.
+     * @param pose
+     * @return 
+     */
+    public Pose remove(Pose pose)
+    {
+        return this;
+    }
+    
+    /**
+     * Adds all bones from the given pose, replacing any old ones.
+     * 
+     * @param pose The pose to copy.
+     * @return This pose.
+     */
+    public Pose put(Pose pose)
+    {
+        for (PoseBone source : pose.bones.values())
         {
-            PoseBone bone = bones.get(target.name);
-            if (target == null) throw new IllegalArgumentException("No such pose bone '" + bone.name + "'");
-            bone.mix(target, t);
+            PoseBone target = bones.get(source.name);
+            if (target == null) bones.put(source.name, target = new PoseBone(source.name));
+            target.transform.set(source.transform);
+        }
+        return this;
+    }
+    
+    /**
+     * Mixes in bones from the given pose. New bones are assumed to mix starting
+     * from the identity transform.
+     * 
+     * @param pose The pose to blend into.
+     * @param blend The factor to blend by.
+     * @return This pose.
+     */
+    public Pose mix(Pose pose, float blend)
+    {
+        for (PoseBone source : pose.bones.values())
+        {
+            PoseBone target = bones.get(source.name);
+            if (target == null) bones.put(source.name, target = new PoseBone(source.name));
+            target.transform.mix(source.transform, blend);
+        }
+        return this;
+    }
+    
+    /**
+     * Class representing the pose transform of a single bone.
+     */
+    public class PoseBone
+    {
+        public final String name;
+        public final Transform transform;
+
+        private PoseBone(DataInputStream in) throws IOException
+        {
+            name = IOUtil.readPaddedUTF(in);
+            transform = new Transform(in);
+        }
+
+        private PoseBone(PoseBone bone)
+        {
+            name = bone.name;
+            transform = new Transform(bone.transform);
         }
         
-        return this;
+        private PoseBone(String name)
+        {
+            this.name = name;
+            transform = new Transform();
+        }
+        
+        /**
+         * Sets a single property value for this bone.
+         * 
+         * @param property The property to change.
+         * @param index The index of the value to change.
+         * @param value Any float.
+         * @return This bone.
+         */
+        public PoseBone setProperty(Property property, int index, float value)
+        {
+            transform.setProperty(property, index, value);
+            return this;
+        }
+
+        /**
+         * Copies the given bone's transform to this.
+         * 
+         * @param bone The bone to copy.
+         * @return This bone.
+         */
+        public PoseBone set(PoseBone bone)
+        {
+            transform.set(bone.transform);
+            return this;
+        }
+        
+        /**
+         * Mixes the given bone's transform into this.
+         * 
+         * @param bone The bone to mix with this.
+         * @param blend The factor to blend by.
+         * @return This bone.
+         */
+        public PoseBone mix(PoseBone bone, float blend)
+        {
+            transform.mix(bone.transform, blend);
+            return this;
+        }
     }
 }
