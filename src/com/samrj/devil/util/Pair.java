5404314961e4effd896242aac22858083e69a5e0
Sam Johnson <x.smashmaster.x@gmail.com>
2016-05-02 13:07:28 -0600

    Added Pair class

---
+++ src/com/samrj/devil/util/Pair.java
@@ -0,0 +1,20 @@
+package com.samrj.devil.util;
+
+/**
+ * Generic pair class.
+ * 
+ * @author Samuel Johnson (SmashMaster)
+ * @copyright 2015 Samuel Johnson
+ * @license https://github.com/SmashMaster/DevilUtil/blob/master/LICENSE
+ */
+public class Pair<A, B>
+{
+    public final A a;
+    public final B b;
+    
+    public Pair(A a, B b)
+    {
+        this.a = a;
+        this.b = b;
+    }
+}
