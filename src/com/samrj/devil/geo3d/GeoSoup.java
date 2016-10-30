7c4bfdf4f54a77e6947ec55c2f061cb153f05299
Sam Johnson <x.smashmaster.x@gmail.com>
2016-10-30 14:55:40 -0600

    Made GeoSoup more extensible

--- src/com/samrj/devil/geo3d/GeoSoup.java
+++ src/com/samrj/devil/geo3d/GeoSoup.java
@@ -1,8 +1,8 @@
 package com.samrj.devil.geo3d;
 
 import com.samrj.devil.math.Vec3;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 /**
@@ -11,23 +11,59 @@ import java.util.stream.Stream;
  * @author Samuel Johnson (SmashMaster)
  * @param <V> The type of vertex this soup contains.
  * @param <E> The type of edge this soup contains.
- * @param <T> The type of triangle this soup contains.
+ * @param <F> The type of face this soup contains.
  * @copyright 2016 Samuel Johnson
  * @license https://github.com/SmashMaster/DevilUtil/blob/master/LICENSE
  */
-public class GeoSoup<V extends Vertex3, E extends Edge3, T extends Triangle3> implements Geometry
+public class GeoSoup<V extends Vertex3, E extends Edge3, F extends Triangle3> implements Geometry
 {
-    public final List<V> verts = new ArrayList<>();
-    public final List<E> edges = new ArrayList<>();
-    public final List<T> faces = new ArrayList<>();
+    private final Supplier<Stream<V>> vertProvider;
+    private final Supplier<Stream<E>> edgeProvider;
+    private final Supplier<Stream<F>> faceProvider;
     
     private final Box3 bounds = Box3.infinite();
     private boolean boundsDirty = true;
-
+    
+    public GeoSoup(Supplier<Stream<V>> vProvider, Supplier<Stream<E>> eProvider, Supplier<Stream<F>> fProvider)
+    {
+        if (vProvider == null) throw new NullPointerException();
+        if (eProvider == null) throw new NullPointerException();
+        if (fProvider == null) throw new NullPointerException();
+        
+        vertProvider = vProvider;
+        edgeProvider = eProvider;
+        faceProvider = fProvider;
+    }
+    
+    public GeoSoup(Collection<V> verts, Collection<E> edges, Collection<F> faces)
+    {
+        this(verts::stream, edges::stream, faces::stream);
+    }
+    
+    public GeoSoup(V[] verts, E[] edges, F[] faces)
+    {
+        this(() -> Stream.of(verts), () -> Stream.of(edges), () -> Stream.of(faces));
+    }
+    
+    public Stream<V> verts()
+    {
+        return vertProvider.get();
+    }
+    
+    public Stream<E> edges()
+    {
+        return edgeProvider.get();
+    }
+    
+    public Stream<F> faces()
+    {
+        return faceProvider.get();
+    }
+    
     @Override
     public Stream<RaycastResult> raycastUnsorted(Vec3 p0, Vec3 dp, boolean terminated)
     {
-        return faces.stream()
+        return faceProvider.get()
                 .map(f -> Geo3DUtil.raycast(f, p0, dp, terminated))
                 .filter(e -> e != null);
     }
@@ -36,9 +72,9 @@ public class GeoSoup<V extends Vertex3, E extends Edge3, T extends Triangle3> im
     public Stream<IsectResult> intersectUnsorted(ConvexShape shape)
     {
         return Stream.concat(Stream.concat(
-                faces.stream().map(f -> shape.isect(f)),
-                edges.stream().map(e -> shape.isect(e))),
-                verts.stream().map(v -> shape.isect(v)))
+                faceProvider.get().map(f -> shape.isect(f)),
+                edgeProvider.get().map(e -> shape.isect(e))),
+                vertProvider.get().map(v -> shape.isect(v)))
                     .filter(e -> e != null);
     }
 
@@ -46,9 +82,9 @@ public class GeoSoup<V extends Vertex3, E extends Edge3, T extends Triangle3> im
     public Stream<SweepResult> sweepUnsorted(ConvexShape shape, Vec3 dp)
     {
         return Stream.concat(Stream.concat(
-                faces.stream().map(f -> shape.sweep(dp, f)),
-                edges.stream().map(e -> shape.sweep(dp, e))),
-                verts.stream().map(v -> shape.sweep(dp, v)))
+                faceProvider.get().map(f -> shape.sweep(dp, f)),
+                edgeProvider.get().map(e -> shape.sweep(dp, e))),
+                vertProvider.get().map(v -> shape.sweep(dp, v)))
                     .filter(e -> e != null);
     }
     
@@ -74,18 +110,18 @@ public class GeoSoup<V extends Vertex3, E extends Edge3, T extends Triangle3> im
     public void updateBounds()
     {
         bounds.setEmpty();
-        for (Vertex3 v : verts) bounds.expand(v.p());
-        for (Edge3 e : edges)
+        vertProvider.get().forEach(v -> bounds.expand(v.p()));
+        edgeProvider.get().forEach(e ->
         {
             bounds.expand(e.a().p());
             bounds.expand(e.b().p());
-        }
-        for (Triangle3 f : faces)
+        });
+        faceProvider.get().forEach(f ->
         {
             bounds.expand(f.a().p());
             bounds.expand(f.b().p());
             bounds.expand(f.c().p());
-        }
+        });
         
         boundsDirty = false;
     }
