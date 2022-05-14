16e4c23c06d9a22d692580eefbfe1dac18872f7c
SmashMaster <sam@samrj.com>
2022-05-13 20:13:31 -0400

    Revert "Pair - Replaced with record"
    This reverts commit e9b97d25f3b1325dabdc4747abbb55e1ced4b4d4.

--- src/com/samrj/devil/util/Pair.java
+++ src/com/samrj/devil/util/Pair.java
@@ -6,7 +6,41 @@ import java.util.Objects;
  * Generic pair class.
  * 
  * @author Samuel Johnson (SmashMaster)
- * @copyright 2022 Samuel Johnson
+ * @copyright 2016 Samuel Johnson
  * @license https://github.com/SmashMaster/DevilUtil/blob/master/LICENSE
  */
-public record Pair<A, B>(A a, B b) {}
+public class Pair<A, B>
+{
+    public final A a;
+    public final B b;
+    
+    public Pair(A a, B b)
+    {
+        this.a = a;
+        this.b = b;
+    }
+    
+    @Override
+    public int hashCode()
+    {
+        int hash = 7;
+        hash = 53*hash + Objects.hashCode(a);
+        hash = 53*hash + Objects.hashCode(b);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Pair<?, ?> other = (Pair<?, ?>)obj;
+        return Objects.equals(a, other.a) && Objects.equals(b, other.b);
+    }
+    
+    @Override
+    public String toString()
+    {
+        return "{" + a + ", " + b + "}";
+    }
+}
