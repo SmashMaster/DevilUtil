f82b484d3e6743483c2928c03a8ec303009e65ff
SmashMaster <sam@samrj.com>
2022-10-22 13:23:47 -0400

    Util - integer clamp method

--- src/com/samrj/devil/math/Util.java
+++ src/com/samrj/devil/math/Util.java
@@ -194,6 +194,13 @@ public final class Util
         return x; //Implicitly handles NaN and the infinites.
     }
 
+    public static int clamp(int x, int min, int max)
+    {
+        if (x < min) return min;
+        if (x > max) return max;
+        return x;
+    }
+
     public static void limitLength(Vec3 v, float maxLength)
     {
         float length = v.length();
